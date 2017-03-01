21c2be0d6167d9d20cd0287b26e12d9b9ce5389b
anyasd123 <eeasd@leeds.ac.uk>
2017-03-01 17:03:01 +0000

    Added some information about imports e.g. import math

--- Functions.py
+++ Functions.py
@@ -33,7 +33,7 @@ def calc_hypo(a,b):
     return hypo
 print calc_hypo(3, 4)
 
-#can square root using **0.5 or import numpy and use sqrt
+#can square root using **0.5 or import numpy or math and use sqrt. Use dir(numpy) or dir(math) to see list of things you can call. Do math import sqrt so you do not need to put math.sqrt each time. Or from math import hypot as euclid (so you name it euclid). Or math import * (so imports everything). However you may be importing something that will overwrite something in your global space. 
 
 #Exercise 3
 def calc_hypo(a,b):
@@ -45,3 +45,22 @@ def calc_hypo(a,b):
         return False
     hypo = (a**2 + b**2)**0.5
     return hypo
+
+#import sys to print sys.version or sys.platform or sys.path or sys.argv (holds command line arguments)
+
+
+
+#RELOAD
+import mylib
+print mylib.x
+33.2
+
+#go into mylib file and change x from 33.2 to "hello"
+
+import my lib
+print mylib.x
+33.2
+
+reload(mylib)
+print mylib.x
+"hello"
