b658bf1885911c31b92fae308956d4b5e6f8ddca
anyasd123 <eeasd@leeds.ac.uk>
2017-03-01 09:42:09 +0000

    Added Operators.py

---
+++ Operators.py
@@ -0,0 +1,35 @@
+age = 23
+name = "Jemma"
+height = 1.63
+ if name == "Jemma" and age >= 23 and height >= 1.63:
+    "It is definitely you Jemma!"
+
+#The use of and means they all must be true
+
+if not name == "Hannah":
+    print "Not allowed in."
+
+#Saying if name !== "Hannah" would mean the same as here
+
+x = 25
+if x not in [1, 2, 3]:
+    print "Didn't find x in list"
+
+greeting = "hello"
+if greeting == "hi" or greeting == "hello":
+    print "Good day to you too"
+
+arg = 5.1
+if type(arg) == str or arg > 5
+   print yikes
+
+start = None
+end = 55
+status = "STARTED"
+
+if status == "STARTED" and (start is not None or end > 0):
+    print "Running."
+#None is equivalent to null (false if you test for it)
+#Would therefore print "Running" as end > 0
+
+#help(name_of_function)
