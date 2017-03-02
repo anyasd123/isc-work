8375a1c27f43d67c5d3702a903c16d5c88066117
anyasd123 <eeasd@leeds.ac.uk>
2017-03-02 10:08:41 +0000

    Added Scripts and Libraries

---
+++ ScriptsandLibraries.py
@@ -0,0 +1,10 @@
+# halman.py
+threshold(signal):
+    return 1.0/sum(signal)
+
+# program.py
+import halman  #do not add file extension when importing (.py)
+readings = [0.1, 0.4, 0.2] 
+
+
+#Can group a set of modules into a package or librarypython
