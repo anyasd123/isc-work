ee71f9689d53c4180ae1567cdf5d90366e431936
anyasd123 <eeasd@leeds.ac.uk>
2017-03-03 14:35:56 +0000

    Added netcdf

---
+++ NetCDF.py
@@ -0,0 +1,23 @@
+#NetCDF4-python module
+
+# TO create a netCDF file call Dataset() constructor. This is also method used to open an existing file. 
+
+from netcdf4 import Dataset
+dataset = Dataset ('data/test.nc', 'w',format = 'NETCDF4_CLASSIC')
+print dataset.file_format
+
+level = dataset.createDimension('level', 10)
+lat = dataset.createDimension('lat', 73)
+lon = dataset.createDimension('lom', 144)
+time = dataset.createDimension('time', None) 
+
+print len(lon)
+print time.issunlimited 
+
+#Create variable
+
+import numpy as np
+times = dataset.createVariable('time', np.float64, ('time',))etc #time could be large number so float64
+#ETC
+
+dataset.description
